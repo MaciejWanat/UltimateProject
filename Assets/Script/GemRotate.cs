781b1342484a488ad6141b45ef8e21ec3285a9f8
Peace <macio-111@o2.pl>
2018-04-04 15:38:41 +0200

    Part 004

--- Assets/Script/GemRotate.cs
+++ Assets/Script/GemRotate.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class GemRotate : MonoBehaviour {
 	
 	public int RotateSpeed; 
+	public AudioSource CollectSound;
+	public GameObject TheGem;
 
 	void Update () 
 	{
 		transform.Rotate(0, RotateSpeed, 0, Space.World);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		CollectSound.Play();
+		TheGem.SetActive(false);
 	}
 }
